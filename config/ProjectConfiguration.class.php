498bf8e01770daf0b0990f9bdee53a8d69448a63
Francisco <francisco000@gmail.com>
2011-07-03 19:36:41 -0400

    se supe ProjectConfiguration para habilitar plugin JqueryReloaded

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -10,5 +10,6 @@ class ProjectConfiguration extends sfProjectConfiguration
     $this->enablePlugins('sfDoctrinePlugin');
     $this->enablePlugins('sfDoctrineGuardPlugin');
     require_once realpath(dirname(__FILE__).'/../lib/vendor/facebook/facebook.php');
+    $this->enablePlugins('sfJqueryReloadedPlugin');
   }
 }
