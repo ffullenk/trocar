c885b80125b9bf1e93573654025cd50480e67cdc
Francisco <francisco000@gmail.com>
2011-07-12 12:12:01 -0400

    cambio en el texto para agregar y remover de wantlist

--- apps/frontend/modules/products/templates/showSuccess.php
+++ apps/frontend/modules/products/templates/showSuccess.php
@@ -1,32 +1,29 @@
 <?php use_helper('jQuery'); ?>
 <table>
   <tbody>
+    
     <tr>
-      <th>Id:</th>
-      <td><?php echo $product->getId() ?></td>
-    </tr>
-    <tr>
-      <th>Name:</th>
+      <th>Nombre del producto:</th>
       <td><?php echo $product->getName() ?></td>
     </tr>
     <tr>
-      <th>Description:</th>
+      <th>Descripcion:</th>
       <td><?php echo $product->getDescription() ?></td>
     </tr>
     <tr>
-      <th>Picture:</th>
-      <td><?php echo $product->getPicture() ?></td>
-    </tr>
+      <th>Foto:</th>
+       <td><?php echo image_tag($product->getPicture(),'size=60x60') ?></td>
+      </tr>
     <tr>
       <th>Link:</th>
       <td><?php echo $product->getLink() ?></td>
     </tr>
     <tr>
-      <th>Brand:</th>
+      <th>Marca:</th>
       <td><?php echo $product->getBrand() ?></td>
     </tr>
     <tr>
-      <th>Model:</th>
+      <th>Modelo:</th>
       <td><?php echo $product->getModel() ?></td>
     </tr>
 
@@ -35,11 +32,9 @@
 
 <hr />
 <?php  if($wantList)
-			echo jq_link_to_remote('remover de WantList', array('url'=> 'wantlist/remove?id='.$product->getId()));
-		else echo jq_link_to_remote('agregar a WantList', array('url'=> 'wantlist/add?id='.$product->getId()));
+			echo jq_link_to_remote('Ya no lo quiero', array('url'=> 'wantlist/remove?id='.$product->getId()));
+		else echo jq_link_to_remote('Lo quiero', array('url'=> 'wantlist/add?id='.$product->getId()));
 		  ?>
 
 &nbsp;
-<a href="<?php echo url_for('products/edit?id='.$product->getId()) ?>">Edit</a>
-&nbsp;
-<a href="<?php echo url_for('products/index') ?>">List</a>
+<a href="<?php echo url_for('products/index') ?>">Volver al indice de productos</a>
