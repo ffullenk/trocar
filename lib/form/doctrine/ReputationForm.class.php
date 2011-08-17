b653b1abb8a0448eed2814df853dbf20795509da
Diego Acuña <diego.desu@gmail.com>
2011-08-16 23:03:13 -0400

    Integración diseño versión 2. Luego se irá agregando la integración con otras vistas.

--- lib/form/doctrine/ReputationForm.class.php
+++ lib/form/doctrine/ReputationForm.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Review form.
+ * Reputation form.
  *
  * @package    trocar
  * @subpackage form
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class ReviewForm extends BaseReviewForm
+class ReputationForm extends BaseReputationForm
 {
   public function configure()
   {
